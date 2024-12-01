500dd05d1163d6ae400c4293157f3daed783d6c5
Vishnu Teja <vishnu.teja.c99@gmail.com>
2024-12-01 21:40:20 +0530

    Initial Avalonia project creation

---
+++ moos/Models/Library.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+
+namespace moos.Models
+{
+    public class Library
+    {
+        public ObservableCollection<Song> LocalLibraryCollection { get; set; }
+
+        public Library() 
+        {
+            LocalLibraryCollection = [];
+        }
+
+        public void LoadLocalCollection(string folderPath)
+        {
+            LocalLibraryCollection = new ObservableCollection<Song>();
+
+            if (Directory.Exists(folderPath))
+            {
+                foreach (string filePath in Directory.GetFiles(folderPath, "*.mp3"))
+                {
+                    Console.WriteLine($"{filePath}");
+                }
+            }
+            else 
+            {
+                throw new DirectoryNotFoundException($"Local folder not found at \"{folderPath}\"");
+            }
+        }
+    }
+}
